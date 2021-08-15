1db3fc44745227a55dc57e39dbf29ace3120ba7a
yulybeltran <yulybeltran082@gmail.com>
2021-08-14 21:57:11 -0500

    14 de agosto 10:00pm se cambio la carpeta del src

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Iniciosesion from './pages/Iniciosesion';
 import SignUp from './pages/SignUp';
 import Listadodeegresados from './pages/Listadodeegresados';
 import InformacionLaboral from './pages/InformacionLaboral';
-import ConsultarSeguimiento from './pages/ConsultarSeguimiento';
 
 function App() {
   return (
@@ -21,7 +19,6 @@ function App() {
           <Route path='/registrar' component={SignUp} />
           <Route path='/listaegresados' component={Listadodeegresados} />
           <Route path='/informacionlaboral' component={InformacionLaboral} />
-          <Route path='/consultarseguimiento' component={ConsultarSeguimiento} />
 
         </Switch>
         
