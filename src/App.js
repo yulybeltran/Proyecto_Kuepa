132b6d68f214b19f115489bdecd0986a457859ca
yulybeltran <yulybeltran082@gmail.com>
2021-08-17 08:32:16 -0500

    Se agrego el basename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, HashRouter} from 'react-router-dom';
 import Home from './pages/Home';
 import Iniciosesion from './pages/Iniciosesion';
 import SignUp from './pages/SignUp';
